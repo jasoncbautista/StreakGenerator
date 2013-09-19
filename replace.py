1af39988e69b1831cc96aee1a0876469ff0dd349
Jason B <bjason@gmail.com>
2013-09-18 22:09:25 -0700

    Replace example working...

--- replace.py
+++ replace.py
@@ -1,10 +1,24 @@
 import re
 
 def replacer(match):
-        return match.group(1).upper()
+        return "ONE"
+
+
+f = open('sampleVC.js')
+lines = f.readlines()
+f.close()
 
 rawString = "{{prefix_HelloWorld}}   testing this. {{_thiswillNotMatch}} {{prefix_Okay}}"
-result = re.sub(r'\{\{prefix_(.+?)\}\}', replacer, rawString)
 
 
-print result
+
+
+
+cleanLines = []
+
+for line in lines:
+
+    result = re.sub(r'\{\{replace_class\}\}', replacer, line)
+    print result
+
+
