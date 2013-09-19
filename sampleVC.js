5977e5cdcde41af95ba9323c90f9607336360f86
Jason B <bjason@gmail.com>
2013-09-18 22:03:01 -0700

    Initializing replace python

---
+++ sampleVC.js
@@ -0,0 +1,29 @@
+(function(Streak) {
+  var $ = Streak.jQuery,
+    Gmail = Streak.Gmail,
+    BB = Streak.BentoBox,
+    HTML = Streak.HTML,
+    Locale = Streak.Locale,
+    _ = Streak._,
+    Date = Streak.Date;
+
+
+  var {{replace_class}}VC = function(options){
+      var self = this;
+      self._delegates = [];
+      // Boxes Button
+      self._view = new BB.Modules.{{replace_class}}View();
+  };
+
+  _.extend({{replace_class}}VC.prototype, {
+
+      getView: function() {
+          var self = this;
+          return self._view;
+      },
+
+      ddfkdsjfsdj: null
+  });
+
+  BB.Modules.{{replace_class}}VC = {{replace_class}}VC;
+})(Streak);
