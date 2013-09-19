8808e038d77e675d1f59fa48ae63e0ac3a6ba623
Jason B <bjason@gmail.com>
2013-09-18 22:36:40 -0700

    Writing different tempaltes

--- replace.py
+++ replace.py
@@ -5,25 +5,40 @@ templateFileName = 'sampleVC.js'
 className = "Cool"
 
 
-# Open up our file
-tempalteFile = open(templateFileName)
-lines = tempalteFile.readlines()
-tempalteFile.close()
-
-# Open a new file
-targetFileName = className + "VC.js"
-targetFile = open(targetFileName, 'w')
-
-# Now we generate the lines with our classname
-cleanLines = []
-for line in lines:
-    result = re.sub(r'\{\{replace_class\}\}', lambda match:  className, line)
-    targetFile.write(result);
-    cleanLines.append(result)
 
 
+def generteJavascriptClassFromTemplate (className, classType):
+    # Open up our file
+    templateFileName = "tempalte" + classType + ".js"
+    tempalteFile = open(templateFileName)
+    lines = tempalteFile.readlines()
+    tempalteFile.close()
+
+    # Open a new file
+    targetFileName = className + classType   + ".js"
+    targetFile = open(targetFileName, 'w')
+
+    # Now we generate the lines with our classname
+    cleanLines = []
+    for line in lines:
+        result = re.sub(r'\{\{replace_class\}\}', lambda match:  className, line)
+        targetFile.write(result);
+        cleanLines.append(result)
 
 
+
+
+generteJavascriptClassFromTemplate(className, "VC")
+generteJavascriptClassFromTemplate(className, "VC")
+
+
+# We need to write our:
+# - ViewController
+# - View
+# - Model
+#
+# - .html
+# - .css
 
 
 
